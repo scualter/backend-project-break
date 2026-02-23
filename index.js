33704e0a7c5590bbab1df45ef4c8deaa6a2420a5
Ruben <rubencarreteropelaez@gmail.com>
2026-02-24 00:25:42 +0100

    Segundo Commit

--- index.js
+++ index.js
@@ -11,10 +11,15 @@ connectDB();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.redirect("/products/view");
+});
+
 app.use("/products", productRoutes);
 
 app.use(errorHandler);
 
-app.listen(process.env.PORT, () => {
-console.log("Servidor corriendo en http://localhost:" + process.env.PORT);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en http://localhost:" + PORT);
 });
