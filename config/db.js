101a4603b72d1352111d141c1ee1c531e7ae78e6
Ruben <rubencarreteropelaez@gmail.com>
2026-02-24 01:10:10 +0100

    Cuarto Commit

--- config/db.js
+++ config/db.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose");
+
 const connectDB = async () => {
-await mongoose.connect(process.env.MONGO_URI);
-console.log("Mongo conectado");
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Mongo conectado");
+  } catch (error) {
+    console.error("Error conectando a Mongo:", error);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
